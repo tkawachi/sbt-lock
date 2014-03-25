1a07d45eefdfb5c0cceff4f2a9474c170b3c2cd9
KAWACHI Takashi <tkawachi@gmail.com>
2014-03-25 14:20:55 +0900

    Show configurations when multiple revisions exist

--- src/main/scala/com/github/tkawachi/SbtLock.scala
+++ src/main/scala/com/github/tkawachi/SbtLock.scala
@@ -1,8 +1,8 @@
 package com.github.tkawachi
 
-import sbt._
-import sbt.Keys._
 import org.apache.maven.artifact.versioning.ComparableVersion
+import sbt.{ IO, File, ModuleID }
+import sbt.Keys.TaskStreams
 
 object SbtLock {
 
@@ -12,10 +12,14 @@ object SbtLock {
   }
 
   def doLock(allModules: Seq[ModuleID], outputFile: File, s: TaskStreams): Unit = {
-    val revisionsMap: Map[Artifact, Set[String]] =
+    val revisionsMap: Map[Artifact, Map[String, Set[String]]] =
       allModules.groupBy { m =>
         Artifact(m.organization, m.name)
-      }.mapValues(_.map(_.revision).toSet)
+      }.mapValues { modulesInArtifact =>
+        modulesInArtifact.groupBy(_.revision).mapValues { modulesInRevision =>
+          modulesInRevision.map(_.configurations).flatten.toSet
+        }
+      }
 
     val moduleLines = revisionsMap.map {
       case (artifact, revisions) =>
@@ -32,12 +36,16 @@ object SbtLock {
     s.log.info(s"$outputFile was created.")
   }
 
-  def chooseRevision(artifact: Artifact, revisions: Set[String], s: TaskStreams): String = {
-    if (revisions.size == 1) revisions.head
+  def chooseRevision(artifact: Artifact, revisions: Map[String, Set[String]], s: TaskStreams): String = {
+    if (revisions.size == 1) revisions.head._1
     else {
-      s.log.info(s"Multiple version exists for $artifact: $revisions")
-      val revision = latest(revisions)
-      s.log.info(s"$revision is chosen.")
+      s.log.info(s"Multiple versions exist for ${artifact.organization} % ${artifact.name}:")
+      revisions.foreach {
+        case (rev, configs) =>
+          s.log.info(s"  * $rev in ${configs.mkString(",")}")
+      }
+      val revision = latest(revisions.keys.toSet)
+      s.log.info(s"  -> $revision is chosen.")
       revision
     }
   }

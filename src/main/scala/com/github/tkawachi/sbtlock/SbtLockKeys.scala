b9e53b1cf8296e197c5902d42984831e0ef9129a
Doug Roper <doug.roper@rallyhealth.com>
2018-12-12 16:19:09 -0500

    Clarify sbtLockHashIsUpToDate description re: transitive project deps.

--- src/main/scala/com/github/tkawachi/sbtlock/SbtLockKeys.scala
+++ src/main/scala/com/github/tkawachi/sbtlock/SbtLockKeys.scala
@@ -8,6 +8,8 @@ object SbtLockKeys {
   val unlock = taskKey[Unit]("Delete a version locking file for sbt-lock")
   val collectLockModuleIDs = taskKey[Seq[ModuleID]]("Collect ModuleIDs to lock")
   val checkLockUpdate = taskKey[Unit]("Check whether a version locking file needs update")
-  val sbtLockHashIsUpToDate = settingKey[Boolean]("lock.sbt file is up to date with libraryDependencies.")
+  val sbtLockHashIsUpToDate = settingKey[Boolean]("lock.sbt file is up to date with libraryDependencies in the current project. " +
+    "(sbtLockHashIsUpToDate in ThisProject) does NOT consider the libraryDependencies of project dependencies! " +
+    "(sbtLockHashIsUpToDate in ThisBuild) considers the libraryDependencies across all projects.")
   val sbtLockIgnoreOverridesOnStaleHash = settingKey[Boolean]("When libraryDependencies are changed, ignores the lock.sbt file.")
 }
